bd695b0fe47c3041ceba4929a688de8b02c0da52
Niemaly <jac3k2@gmail.com>
2020-05-12 14:48:55 +0200

    this is not a good impl for big arrays

--- src/test/java/src/asseco/TestForAsseco.java
+++ src/test/java/src/asseco/TestForAsseco.java
@@ -4,16 +4,30 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestForAsseco {
 
+
+
+
+
+
     private static Stream<Arguments> provideDataForTaskNr1() {
+        int a[] =new int[100000];
+
+
+        for(int i=0 ; i<a.length; i++){
+            a[i]=i+1;
+        }
         return Stream.of(
                 Arguments.of(new int[]{1, 3, 6, 4, 1, 2}, 5),
                 Arguments.of(new int[]{1, 2, 3}, 4),
-                Arguments.of(new int[]{-1, -3}, 1)
+                Arguments.of(new int[]{-1, -3}, 1),
+                Arguments.of(a , 100001)
+
 
         );
     }
